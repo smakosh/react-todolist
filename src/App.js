51f46254a60ac9ca6f68670087c9ec43e38f3969
ismail ghallou <ismai23l@hotmail.com>
2017-11-01 13:06:07 +0000

    states & props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import React from 'react';
+//import PropTypes from 'prop-types';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            text: 'this is the state text',
+            counter: 0
+        }
+    }
+    update( e ) {
+        this.setState({text: e.target.value})
+    }
+    increment ( e ) {
+        this.setState(
+            { counter: e.target.value++ } 
+        )
+    }
     render() {
-        return <h1>Hello world !</h1>
+        //let data = this.props.data
+        //let counter = this.props.counter
+        return (
+            <div>
+                <h1>{this.state.text}</h1>
+                <input 
+                    type='text'
+                    onChange={this.update.bind(this)} />
+                <p>{this.state.counter}</p>
+                <button onClick={this.increment.bind(this)} value="1">Increment</button>
+            </div>
+        )
     }
 }
+
+//App.propTypes = {
+//    data: PropTypes.string,
+//    counter: PropTypes.number.isRequired
+//}
+
+//App.defaultProps = {
+//    data: "default text"
+//}
 
 export default App
