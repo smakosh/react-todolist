6d9a93a4da199feee2593e065e137d11a0a5a3e1
smakosh <ismai23l@hotmail.com>
2017-11-09 13:09:15 +0000

    handling events

--- src/SingleTask.js
+++ src/SingleTask.js
@@ -12,16 +12,36 @@ export default class SingleTask extends Component {
         var tasks = this.state.tasks;
         tasks = tasks.map(function(name, index){
             return (
-                <div className="single-task" key={index}>
-                    <h4 >{name}</h4>
-                    <a href="#"><i className="fa fa-close"></i></a>                
-                </div>
+                <SingleItem key={index} name={name} deleteTask={this.deleteTask}/>
             )
-        })
+        }.bind(this));
         return (
             <div className="card">
                 {tasks}
             </div>
         );
+    }
+    deleteTask(name) {
+        var tasksup = this.state.tasks.filter(function(val, index) {
+            return name !== val;
+        })
+        this.setState({
+            tasks: tasksup
+        })
+    }
+}
+
+class SingleItem extends Component {
+
+    render() {
+        return (
+            <div className="single-task">
+                <h4 >{this.props.name}</h4>
+                <a onClick={this.deleteSend.bind(this)}><i className="fa fa-close"></i></a>                
+            </div>
+        );
+    }
+    deleteSend(name) {
+        this.props.deleteTask(this.props.name);
     }
 }
